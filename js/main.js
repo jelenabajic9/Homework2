3ca83d656f9cdc2f6814e41bdac8e0a4d117f429
jelenabajic9 <bajickica22@gmail.com>
2023-01-05 21:16:37 +0100

    Done responsive design for whole page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+function toggleBurger(i) {
+  const html = document.querySelector("html");
+  const body = document.querySelector("body");
+  const nav = document.querySelector(".header");
+  nav.classList.toggle("open");
+  i.classList.toggle("is-active");
+  nav.classList.toggle("scroll");
+  html.classList.toggle("no-scroll");
+  body.classList.toggle("no-scroll");
+}
+
+
 $(function () {
     $(document).scroll(function () {
         var nav = $(".header");
